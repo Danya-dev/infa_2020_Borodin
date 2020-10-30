9dbdadabc96e87378b3b68e96d139acc66961f3a
Danya <borodin.da@phystech.edu>
2020-10-30 11:18:42 +0300

    manager is done

--- gun/untitled6.py
+++ gun/untitled6.py
@@ -152,22 +152,96 @@ class Table():
 
 
 class Manager():
-    def __init__(self):
-        self.gun = Gun()
-        self.score_t = Table()
-    def draw(self, screen):
-        screen.fill(BLACK)
     
+    def __init__(self, n_targets=1):
+        self.balls = []
+        self.gun = Gun()
+        self.targets = []
+        self.score_t = ScoreTable()
+        self.n_targets = n_targets
+        self.new_mission()
+        
+        
+    def new_mission(self):
+        
+        for i in range(self.n_targets):
+            self.targets.append(Target(rad=randint(max(1, 30 - 2*max(0, self.score_t.score())), 
+                30 - max(0, self.score_t.score()))))
+            
     def process(self, events, screen):
+        
         done = self.handle_events(events)
+
+        if pg.mouse.get_focused():
+            mouse_pos = pg.mouse.get_pos()
+            self.gun.set_angle(mouse_pos)
+        
+        self.move()
+        self.collide()
         self.draw(screen)
+
+        if len(self.targets) == 0 and len(self.balls) == 0:
+            self.new_mission()
+
         return done
+    
+    
     def handle_events(self, events):
+     
         done = False
         for event in events:
-            if event.type == pygame.QUIT:
-                done = True 
-        return done
+            if event.type == pg.QUIT:
+                done = True
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    self.gun.move(-5)
+                elif event.key == pg.K_DOWN:
+                    self.gun.move(5)
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.gun.activate()
+            elif event.type == pg.MOUSEBUTTONUP:
+                if event.button == 1:
+                    self.balls.append(self.gun.strike())
+                    self.score_t.b_used += 1
+        return done    
+    
+    def draw(self, screen):
+       
+        for ball in self.balls:
+            ball.draw(screen)
+        for target in self.targets:
+            target.draw(screen)
+        self.gun.draw(screen)
+        self.score_t.draw(screen)
+        
+    def move(self):
+
+        dead_balls = []
+        for i, ball in enumerate(self.balls):
+            ball.move(grav=2)
+            if not ball.is_alive:
+                dead_balls.append(i)
+        for i in reversed(dead_balls):
+            self.balls.pop(i)
+        self.gun.gain()
+        
+    def collide(self):
+
+        collisions = []
+        targets_c = []
+        for i, ball in enumerate(self.balls):
+            for j, target in enumerate(self.targets):
+                if target.check_collision(ball):
+                    collisions.append([i, j])
+                    targets_c.append(j)
+        targets_c.sort()
+        for j in reversed(targets_c):
+            self.score_t.t_destr += 1
+            self.targets.pop(j)
+            
+screen = pg.display.set_mode(SCREEN_SIZE)
+pg.display.set_caption("The gun of Khiryanov")
 
 mgr = Manager()
 
