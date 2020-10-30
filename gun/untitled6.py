a0af66eb2b57dd391e3cbb1707d399909749c1aa
Danya <borodin.da@phystech.edu>
2020-10-30 11:13:37 +0300

    table featured

--- gun/untitled6.py
+++ gun/untitled6.py
@@ -137,6 +137,18 @@ class Table():
         self.t_destr = t_destr
         self.b_used = b_used
         self.font = pg.font.SysFont("dejavusansmono", 25)
+        
+    def score(self):
+       
+        return self.t_destr - self.b_used
+    
+    def draw(self, screen):
+        score_surf = []
+        score_surf.append(self.font.render("Destroyed: {}".format(self.t_destr), True, WHITE))
+        score_surf.append(self.font.render("Balls used: {}".format(self.b_used), True, WHITE))
+        score_surf.append(self.font.render("Total: {}".format(self.score()), True, RED))
+        for i in range(3):
+            screen.blit(score_surf[i], [10, 10 + 30*i])
 
 
 class Manager():
